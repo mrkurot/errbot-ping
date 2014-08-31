d2cd59453e2eecd2d6fe1b7ffdbf957bf1caeaf9
Tom Neyland <tcneyland+github@gmail.com>
2014-08-31 13:52:53 -0400

    Update svsl.py

--- svsl.py
+++ svsl.py
@@ -4,9 +4,9 @@ from errbot.builtins.webserver import webhook
 
 class SVSL(BotPlugin):
 
-    """A PingSVSL function for Err"""
+    """A Ping SVSL function for Err"""
     min_err_version = '1.6.0'  # Optional, but recommended
-    #max_err_version = '2.0.0'  # Optional, but recommended
+    max_err_version = '3.0.0'  # Optional, but recommended
 
     @botcmd(split_args_with=None)
     def svsl(self, mess, args):
