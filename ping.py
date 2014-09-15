5fad3b82dca0d443c3dcb812e34c7d6ebb24361b
Tom Neyland <tcneyland+github@gmail.com>
2014-09-15 01:54:35 -0400

    Update and rename svsl.py to ping.py

---
+++ ping.py
@@ -0,0 +1,39 @@
+from errbot import BotPlugin, botcmd
+from errbot.builtins.webserver import webhook
+
+
+class Ping(BotPlugin):
+
+    """A Ping Group function for Err"""
+    min_err_version = '1.6.0'  # Optional, but recommended
+    #max_err_version = '3.0.0'  # Optional, but recommended
+
+    groups = {}
+
+    @botcmd(split_args_with="||")
+    def ping_set(self, mess, args):
+
+        group_str = args[0]
+        group_tuple = group_str.split(" ")
+        
+        if len(group_tuple) > 1:
+            group, text = group_tuple
+        else:
+            del self.groups[group]
+            
+        self.groups[group] = text
+
+        return "Updated group."
+        
+        
+    @botcmd(split_args_with=None)
+    def ping(self, mess, args):
+        
+        group = args[0]
+        
+        group_text = self.groups.get(group, None)
+        
+        if group_text != None:
+            return group_text
+        else:
+            return "No such group"
