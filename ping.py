2464016f77e3a8bffcc1a249de9890d2a3b70199
Thomas Neyland <tcneyland+github@gmail.com>
2014-10-01 05:34:16 +0000

    adds ping_all

--- ping.py
+++ ping.py
@@ -1,24 +1,59 @@
 from errbot import BotPlugin, botcmd
 from errbot.builtins.webserver import webhook
+from collections import defaultdict
 
 
 class Ping(BotPlugin):
 
     """A Ping Group function for Err"""
     min_err_version = '1.6.0'  # Optional, but recommended
-    #max_err_version = '3.0.0'  # Optional, but recommended
+    # max_err_version = '3.0.0'  # Optional, but recommended
+
+    presences = defaultdict(dict)
+
+    def callback_presence(self, presence):
+
+        presences = self.presences
+
+        if presence.chatroom:
+            room = presence.chatroom.node
+            user = presence.nick
+            status = presence.status
+
+            if status == 'online':
+                presences[room][user] = presence
+            elif user in presences[room]:
+                del presences[room][user]
+
+        # if status == ONLINE:
+        #     presence[room][user] = presence
+        # elif user in presence[room]:
+        #     del presence[room][user]
+
+    @botcmd
+    def ping_all(self, mess, args):
+
+        room = mess.getFrom().getNode()
+        usernames = sorted(self.presences[room].keys())
+
+        print room
+        print vars(mess)
+        print self.presences.keys()
+        print usernames
+
+        return " ".join(usernames)
 
     @botcmd(split_args_with="||")
     def ping_set(self, mess, args):
         """Create/Update a group."""
-        
+
         group_str = str(args[0])
         group_tuple = group_str.split(" ", 1)
-        
+
         if len(group_tuple) > 1:
             group, text = group_tuple
             group = group.lower()
-            
+
             old_value = self[group]
             self[group] = text
             if old_value:
@@ -31,49 +66,49 @@ class Ping(BotPlugin):
             if group in self.keys():
                 del self[group]
                 return "Deleted group %s" % group
-        
+
     @botcmd(split_args_with=' ')
     def ping_add(self, mess, args):
         """Append something to a ping group"""
-        
+
         if len(args) > 1:
             group = str(args[0]).lower()
             message = self[group] or ""
             message = message + " " + " ".join(args[1:])
             self[group] = message
             return "Done."
-    
+
     @botcmd(split_args_with=' ')
     def ping_remove(self, mess, args):
         """Remove something from a ping group"""
-        
+
         if len(args) > 1:
             group = str(args[0]).lower()
             message = self[group] or ""
             term = " ".join(args[1:])
             self[group] = message.replace(term, "")
             return "Done."
-        
+
     @botcmd(split_args_with=None)
     def ping(self, mess, args):
         """Ping a specified group"""
-        
+
         group = str(args[0])
         group = group.lower()
-        
+
         group_text = self[group]
-        
+
         if group_text != None:
             return group_text
         else:
             return "No such group, valid groups are: %s" % (", ".join(sorted(self.keys())),)
-            
+
     @botcmd(split_args_with=None)
     def ping_groups(self, mess, args):
         """Show the groups that can be pinged"""
-        
+
         groups = self.keys()
-        
+
         return ", ".join(sorted(groups))
 
     def __getitem__(self, key):
